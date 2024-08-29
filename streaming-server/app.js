4f317609c0a0e85ad5a17945e01bf60af802b093
Sergey Usenko <sergey.usenko@ite.group>
2024-08-29 11:41:12 +0300

    refactoring

--- streaming-server/app.js
+++ streaming-server/app.js
@@ -1,134 +1,113 @@
-// create express app to handle streming request
-const express = require("express")
+const express = require('express');
 const Docker = require('dockerode');
-const dotenv = require("dotenv");
+const dotenv = require('dotenv');
 const axios = require('axios');
 const cors = require('cors');
-const fs = require('fs');
-const constructUrl = require('./utils/construct-url')
-const xmlToJson = require('./utils/xmltojson')
+const constructUrl = require('./utils/construct-url');
+const xmlToJson = require('./utils/xmltojson');
 
+class BBBStreamService {
+  constructor() {
+    this.docker = new Docker();
+    this.bbb = {
+      host: process.env.BBB_URL,
+      salt: process.env.BBB_SECRET,
+    };
+  }
 
+  async startStream(req, res) {
+    try {
+      const { meetingId, hidePresentation, rtmpUrl } = req.body;
 
+      const meetingInfo = await axios.get(
+        constructUrl(this.bbb, 'getMeetingInfo', { meetingID: meetingId })
+      );
+      const info = xmlToJson(meetingInfo.data);
+      const attendeePassword = info.response.attendeePW[0];
 
-const app = express();
-const docker = new Docker();
-dotenv.config();
+      const envVariables = {
+        MEETING_ID: meetingId,
+        ATTENDEE_PW: attendeePassword,
+        HIDE_PRESENTATION: hidePresentation,
+        RTMP_URL: rtmpUrl,
+      };
 
-app.use(express.json({ limit: '1mb' }));
-app.use(express.urlencoded({ extended: true, limit: '1mb' }));
-app.use(express.static('public'));
-app.use(cors());
+      const image = 'bbb-stream:v1.0';
+      const containers = await this.docker.listContainers({ all: true });
+      const streamingContainers = containers.filter((container) => container.Image === image);
 
-
-const bbb = {
-  host: process.env.BBB_URL,
-  salt: process.env.BBB_SECRET,
-};
-
-
-app.post('/bot/start', async (req, res) => {
-  try {
-    const {
-      meetingId,
-      hidePresentation,
-      rtmpUrl
-    } = req.body;
-
-    // get meeting info from bbb 
-    const params = {
-      meetingID: meetingId,
-    }
-    const config = {
-      headers: {
-        'Content-Type': 'application/json'
+      if (streamingContainers.length >= process.env.NUMBER_OF_CONCURRENT_STREAMINGS) {
+        return res
+          .status(500)
+          .json({ error: 'Слишком много потоков: Вы достигли лимита потоков. Попробуйте позже.' });
       }
-    }
 
-    const meetingInfo = await axios.get(constructUrl(bbb, 'getMeetingInfo', params), config)
+      const containerName = `bbb-stream-${meetingId}`;
+      const hostConfig = {
+        Binds: ['/var/run/docker.sock:/var/run/docker.sock'],
+        AutoRemove: true,
+      };
 
-    const info = xmlToJson(meetingInfo.data)
-
-    const attendeePassword = info.response.attendeePW[0];
-
-    const envVariables = {
-      MEETING_ID: meetingId,
-      ATTENDEE_PW: attendeePassword,
-      HIDE_PRESENTATION: hidePresentation,
-      RTMP_URL: rtmpUrl
-    };
-
-    const image = 'bbb-stream:v1.0';
-
-    let containers = await docker.listContainers({ all: true });
-    let streamingContainers = containers.filter(container => container.Image === image);
-
-    if (streamingContainers.length >= process.env.NUMBER_OF_CONCURRENT_STREAMINGS) {
-      return res.status(500).json({ error: `Too Many Streams: You've reached the streaming limit. Check back in a while to continue.` });
-    }
-
-    const containerName = `bbb-stream-${meetingId}`; // create a unique container name using the meetingId
-
-    const hostConfig = {
-      Binds: ['/var/run/docker.sock:/var/run/docker.sock'], // Mount the Docker socket
-      AutoRemove: true,
-    };
-
-    docker.createContainer(
-      {
+      const container = await this.docker.createContainer({
         Image: image,
         name: containerName,
         Env: Object.entries(envVariables).map(([name, value]) => `${name}=${value}`),
         Tty: false,
         HostConfig: hostConfig,
-      },
-      (err, container) => {
-        if (err) {
-          console.error('Error creating container:', err);
-          return res.status(500).json({ error: 'Internal Server Error' });
-        }
+      });
 
-        container.start((err) => {
-          if (err) {
-            console.error('Error starting container:', err);
-            return res.status(500).json({ error: 'Internal Server Error' });
-          }
-
-          console.log('Stream started successfully');
-          return res.status(200).json({ message: 'Stream started successfully' });
-        });
-      }
-    );
-
-  } catch (error) {
-    return res.status(500).json({ error });
+      await container.start();
+      console.log('Поток успешно запущен');
+      return res.status(200).json({ message: 'Поток успешно запущен' });
+    } catch (error) {
+      console.error('Ошибка при запуске потока:', error);
+      return res.status(500).json({ error: 'Внутренняя ошибка сервера' });
+    }
   }
-});
 
+  async stopStream(req, res) {
+    try {
+      const { meetingId } = req.body;
+      const containerName = `bbb-stream-${meetingId}`;
+      const container = this.docker.getContainer(containerName);
 
-app.post('/bot/stop', async (req, res) => {
-  try {
-    const {
-      meetingId,
-    } = req.body;
+      await container.remove({ force: true });
+      return res.status(200).json({ message: 'Поток успешно остановлен' });
+    } catch (error) {
+      console.error('Ошибка при остановке потока:', error);
+      return res.status(500).json({ error: 'Ошибка при остановке потока' });
+    }
+  }
+}
 
-    const containerName = `bbb-stream-${meetingId}`;
-    const container = docker.getContainer(containerName);
+class App {
+  constructor() {
+    this.app = express();
+    this.bbbStreamService = new BBBStreamService();
+    this.setupMiddleware();
+    this.setupRoutes();
+  }
 
-    container.remove({ force: true }, function (err) {
-      if (err) {
-        console.log('Error in stop streaming:', err);
-        return res.status(500).json({ error: 'Error in stop streaming' });
-      }
+  setupMiddleware() {
+    this.app.use(express.json({ limit: '1mb' }));
+    this.app.use(express.urlencoded({ extended: true, limit: '1mb' }));
+    this.app.use(express.static('public'));
+    this.app.use(cors());
+  }
 
+  setupRoutes() {
+    this.app.post('/bot/start', (req, res) => this.bbbStreamService.startStream(req, res));
+    this.app.post('/bot/stop', (req, res) => this.bbbStreamService.stopStream(req, res));
+  }
 
-      return res.status(200).json({ message: 'Stream stopped successfully' });
+  start() {
+    const PORT = 4500;
+    this.app.listen(PORT, () => {
+      console.log(`Сервер запущен на порту ${PORT}`);
     });
-  } catch (error) {
-
   }
-})
+}
 
-const server = app.listen(4500, () => {
-  console.log("Server running on port 4500");
-})
+dotenv.config();
+const app = new App();
+app.start();
